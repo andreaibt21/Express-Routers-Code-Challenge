964d99bde42be2cf8f0bdd583739ba95c7d414cd
andreaibt21 <andreaixabella@gmail.com>
2020-07-19 21:53:19 -0300

    Update app7.js

--- app7.js
+++ app7.js
@@ -1,6 +1,10 @@
-/*Write a route to handle DELETE requests to /puddings/:flavor. It should delete the correct pudding and send a 204 response if the pudding name exists and send a 404 response if it does not.
+/*Write a route to handle DELETE requests to /puddings/:flavor. 
+It should delete the correct pudding and send a 204 response if 
+the pudding name exists and send a 404 response if it does not.
 
-You can use the helper functions findPuddingIndex to find the index of the pudding by flavor and deletePuddingAtIndex to delete a pudding from the puddingFlavors array by index.*/
+You can use the helper functions findPuddingIndex to find the index
+of the pudding by flavor and deletePuddingAtIndex to delete a 
+pudding from the puddingFlavors array by index.*/
 
 const express = require('express');
 const app = express();
@@ -20,16 +24,17 @@ const deletePuddingAtIndex = (index) => {
 const puddingsRouter = express.Router();
 app.use('/puddings', puddingsRouter);
 
-
-puddingsRouter.delete('/:flavor', (req, res, next) => {
-  const puddingIndex = findPuddingIndex(req.params.flavor);
-  if (puddingIndex !== -1) {
+puddingRouter.delete('/:flavor', (req, res, next) => {
+ //find the index puddin and called it puddingIndex
+ const puddingIndex = findPuddingIndex(req.params.flavor);
+ //if the index is not -1 delete the pudding saved on puddingIndex
+ if (puddingIndex !== -1) {
     deletePuddingAtIndex(puddingIndex);
-    res.status(204).send(puddingFlavors);
-  } else {
-    res.status(404).send();
-  }
-})
+   res.status(204).send()
+ } else {
+   res.status(404).send();
+ }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
